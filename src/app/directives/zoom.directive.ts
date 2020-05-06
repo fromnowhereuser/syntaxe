cf460b9f1a13e3c9ffad71df2afa17965e6fa06d
charles <38671736+fromnowhereuser@users.noreply.github.com>
2020-05-06 10:17:40 +0200

    directive zoom ok

--- src/app/directives/zoom.directive.ts
+++ src/app/directives/zoom.directive.ts
@@ -1,10 +1,42 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, Output, EventEmitter, HostListener, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appZoom]'
 })
-export class ZoomDirective {
+export class ZoomDirective implements OnInit {
 
-  constructor() { }
+  constructor(private ref: ElementRef<HTMLElement>) {
+    console.log('ZoomDirective');
+
+  }
+
+  ngOnInit(): void {
+    this.normalSize = this.ref.nativeElement.style.fontSize.match(/\d+/)[0];
+    console.log(this.normalSize);
+  }
+
+  private normalSize;
+
+  @HostListener('mouseover')
+  private onmouseover() {
+
+    this.ref.nativeElement.style.fontSize = (this.normalSize * 2) + 'px';
+  }
+
+
+  @HostListener('mouseleave')
+  private onmouseleave() {
+    this.ref.nativeElement.style.fontSize = (this.normalSize) + 'px';
+  }
+
+  @HostListener('dblclick')
+  private onDblClick() {
+    console.log('dblclick catched');
+  }
+
+
+
+
+
 
 }
